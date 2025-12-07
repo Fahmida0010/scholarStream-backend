b71517d11ceca160495ebfe07e56624ec5aa1a7d
Fahmida0010 <fahmidaakter0010@gmail.com>
2025-12-08 01:04:07 +0600

    scholarship collection added

--- index.js
+++ index.js
@@ -60,13 +60,88 @@ async function run() {
     
     const db = client.db("ScholarStream");
     const usersCollection = db.collection("users");
+  const scholarshipCollection = db.collection("scholarships");
+   const reviewCollection = db.collection("reviews");
 
 
 
 
+   //top scholarships
+  app.get("/top-scholarships", async (req, res) => {
+      const result = await scholarshipCollection
+        .find()
+        .sort({ applicationFees: 1, scholarshipPostDate: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+
+  // ✔ GET SINGLE SCHOLARSHIP DETAILS (app.get rule)
+    app.get("/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await scholarshipCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+
+    // GET ALL SCHOLARSHIPS with optional search & filter
+    // -------------------------------
+    app.get("/scholarships", async (req, res) => {
+      const { search, category, subject, location } = req.query;
+      const query = {};
+
+      if (search) {
+        query.$or = [
+          { scholarshipName: { $regex: search, $options: "i" } },
+          { universityName: { $regex: search, $options: "i" } },
+          { degree: { $regex: search, $options: "i" } },
+        ];
+      }
+      if (category) query.scholarshipCategory = category;
+      if (subject) query.subjectCategory = subject;
+      if (location) query.universityCountry = location;
+
+      const scholarships = await scholarshipCollection.find(query).toArray();
+      res.send(scholarships);
+    });
+
+    // -------------------------------
+    // GET SINGLE SCHOLARSHIP DETAILS
+    // -------------------------------
+    app.get("/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const scholarship = await scholarshipCollection.findOne({ _id: new ObjectId(id) });
+      res.send(scholarship);
+    });
+
+    // -------------------------------
+    // GET REVIEWS BY SCHOLARSHIP/UNIVERSITY ID
+    // -------------------------------
+    app.get("/scholarships/:id/reviews", async (req, res) => {
+      const id = req.params.id;
+      const reviews = await reviewCollection
+        .find({ scholarshipId: id })
+        .sort({ date: -1 })
+        .toArray();
+      res.send(reviews);
+    });
+
+    // -------------------------------
+    // POST REVIEW (Create)
+    // -------------------------------
+    app.post("/scholarship/:id/review", async (req, res) => {
+      const id = req.params.id;
+      const review = req.body; // { reviewerName, reviewerImage, rating, comment, date }
+      review.scholarshipId = id;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
 
 
 
+     //server run
     app.get("/", (req, res) => {
       res.send("ScholarStream server Running Successfully!");
     });
